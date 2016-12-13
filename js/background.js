b211dd17bf4e41b967aa4bb9a3e3d4c12efa2d10
sonack <1243535683@qq.com>
2016-12-13 22:43:46 +0800

    完成v0.6.0的js部分，v0.6.0基本完成

--- js/background.js
+++ js/background.js
@@ -1,28 +1,124 @@
 // 背景页面
 // 检查视图
 
-
 // 主要包括     "上下文菜单" (Context Menu)
 //             "消息传递机制"
-
-
 // 默认enabled=false
 var ctx = ["page", "selection", "link", "editable"];
 var menu = {};
-menu.root          = chrome.contextMenus.create({"title": "表格数据抽取...",                              "enabled": false, "contexts": ctx});
-menu.selectRow     = chrome.contextMenus.create({ "title": "选择一行",    "parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("selectRow")    }});
-menu.selectColumn  = chrome.contextMenus.create({ "title": "选择一列", "parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("selectColumn") }});
-menu.selectTable   = chrome.contextMenus.create({ "title": "选择表格",  "parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("selectTable")  }});
-menu.break1        = chrome.contextMenus.create({ "type": "separator",      "parentId": menu.root, "enabled": false, "contexts": ctx });
-menu.findPrevTable = chrome.contextMenus.create({ "title": "上一个表格","parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("findPrevTable")  }});
-menu.findNextTable = chrome.contextMenus.create({ "title": "下一个表格",    "parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("findNextTable")  }});
-menu.break2        = chrome.contextMenus.create({ "type": "separator",      "parentId": menu.root, "enabled": false, "contexts": ctx });
-menu.copyRich      = chrome.contextMenus.create({ "title": "拷贝",          "parentId": menu.root, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("copyRich")     }});
-menu.copy          = chrome.contextMenus.create({ "title": "拷贝为...",    "parentId": menu.root, "enabled": false, "contexts": ctx });
-menu.copyHTML      = chrome.contextMenus.create({ "title": "简单HTML",          "parentId": menu.copy, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("copyHTML")     }});
-menu.copyStyled    = chrome.contextMenus.create({ "title": "带样式的HTML",   "parentId": menu.copy, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("copyStyled")  }});
-menu.copyCSV       = chrome.contextMenus.create({ "title": "CSV格式",           "parentId": menu.copy, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("copyCSV")      }});
-menu.copyText      = chrome.contextMenus.create({ "title": "纯文本",     "parentId": menu.copy, "enabled": false, "contexts": ctx, "onclick": function() { menuClick("copyText")     }});
+menu.root = chrome.contextMenus.create({
+    "title": "表格数据抽取...",
+    "enabled": false,
+    "contexts": ctx
+});
+menu.selectRow = chrome.contextMenus.create({
+    "title": "选择一行",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("selectRow")
+    }
+});
+menu.selectColumn = chrome.contextMenus.create({
+    "title": "选择一列",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("selectColumn")
+    }
+});
+menu.selectTable = chrome.contextMenus.create({
+    "title": "选择表格",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("selectTable")
+    }
+});
+menu.break1 = chrome.contextMenus.create({
+    "type": "separator",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx
+});
+menu.findPrevTable = chrome.contextMenus.create({
+    "title": "上一个表格",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("findPrevTable")
+    }
+});
+menu.findNextTable = chrome.contextMenus.create({
+    "title": "下一个表格",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("findNextTable")
+    }
+});
+menu.break2 = chrome.contextMenus.create({
+    "type": "separator",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx
+});
+menu.copyRich = chrome.contextMenus.create({
+    "title": "拷贝",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("copyRich")
+    }
+});
+menu.copy = chrome.contextMenus.create({
+    "title": "拷贝为...",
+    "parentId": menu.root,
+    "enabled": false,
+    "contexts": ctx
+});
+menu.copyHTML = chrome.contextMenus.create({
+    "title": "简单HTML",
+    "parentId": menu.copy,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("copyHTML")
+    }
+});
+menu.copyStyled = chrome.contextMenus.create({
+    "title": "带样式的HTML",
+    "parentId": menu.copy,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("copyStyled")
+    }
+});
+menu.copyCSV = chrome.contextMenus.create({
+    "title": "CSV格式",
+    "parentId": menu.copy,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("copyCSV")
+    }
+});
+menu.copyText = chrome.contextMenus.create({
+    "title": "纯文本",
+    "parentId": menu.copy,
+    "enabled": false,
+    "contexts": ctx,
+    "onclick": function() {
+        menuClick("copyText")
+    }
+});
 
 // 向标签页发送消息
 // @Params
@@ -30,10 +126,18 @@ menu.copyText      = chrome.contextMenus.create({ "title": "纯文本",     "par
 //      broadcast 若为true, 则广播给所有tabs; 若为false, 只传递给当前窗口的活动标签页
 //      fn 指定回调函数，默认为空
 function sendCommand(cmd, broadcast, fn) {
-    var qry = broadcast ? {} : {active: true, currentWindow: true};
-    chrome.tabs.query(qry, function(tabs) {
+    var qry = broadcast ? {}: {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(qry,
+    function(tabs) {
         tabs.forEach(function(tab) {
-            chrome.tabs.sendMessage(tab.id, {command: cmd}, fn || function(r) {});
+            chrome.tabs.sendMessage(tab.id, {
+                command: cmd
+            },
+            fn ||
+            function(r) {});
         });
     });
 }
@@ -48,101 +152,121 @@ chrome.tabs.onActivated.addListener(function() {
     sendCommand("activate");
 });
 
-
 // 处理来自content.js消息命令
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    switch(message.command) {
+    switch (message.command) {
 
-        case "copyText":
-        case "copyHTML":
-        case "copyStyled":
-        case "copyCSV":
-            var t = document.getElementById("___copytables_clipboard___");
-            t.value = message.content;
-            t.focus();
-            t.select();
-            document.execCommand("copy");
-            t.value = "";
-            sendResponse({});
-            break;
+    case "copyText":
+    case "copyHTML":
+    case "copyStyled":
+    case "copyCSV":
+        var t = document.getElementById("___copytables_clipboard___");
+        t.value = message.content;
+        t.focus();
+        t.select();
+        document.execCommand("copy");
+        t.value = "";
+        sendResponse({});
+        break;
 
-        case "copyRich":
-            var t = document.getElementById("___copytables_div___");
-            t.contentEditable = true;
-            t.innerHTML = message.content;
-            var range = document.createRange();
-            range.selectNodeContents(t);
-            var selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            document.execCommand("copy");
-            t.innerHTML = "";
-            sendResponse({});
-            break;
+    case "copyRich":
+        var t = document.getElementById("___copytables_div___");
+        t.contentEditable = true;
+        t.innerHTML = message.content;
+        var range = document.createRange();
+        range.selectNodeContents(t);
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand("copy");
+        t.innerHTML = "";
+        sendResponse({});
+        break;
 
-        case "menuUpdate":
+    case "menuUpdate":
 
-            var s = message.state;
+        var s = message.state;
 
-            chrome.contextMenus.update(menu.root,           {enabled:s.hasTables});
+        chrome.contextMenus.update(menu.root, {
+            enabled: s.hasTables
+        });
 
-            chrome.contextMenus.update(menu.selectRow,      {enabled:s.canSelect});
-            chrome.contextMenus.update(menu.selectColumn,   {enabled:s.canSelect});
-            chrome.contextMenus.update(menu.selectTable,    {enabled:s.canSelect});
+        chrome.contextMenus.update(menu.selectRow, {
+            enabled: s.canSelect
+        });
+        chrome.contextMenus.update(menu.selectColumn, {
+            enabled: s.canSelect
+        });
+        chrome.contextMenus.update(menu.selectTable, {
+            enabled: s.canSelect
+        });
 
-            chrome.contextMenus.update(menu.findPrevTable,  {enabled:s.hasTables});
-            chrome.contextMenus.update(menu.findNextTable,  {enabled:s.hasTables});
+        chrome.contextMenus.update(menu.findPrevTable, {
+            enabled: s.hasTables
+        });
+        chrome.contextMenus.update(menu.findNextTable, {
+            enabled: s.hasTables
+        });
 
-            chrome.contextMenus.update(menu.copyRich,       {enabled:s.canCopy});
-            chrome.contextMenus.update(menu.copy,           {enabled:s.canCopy});
-            chrome.contextMenus.update(menu.copyHTML,       {enabled:s.canCopy});
-            chrome.contextMenus.update(menu.copyStyled,     {enabled:s.canCopy});
-            chrome.contextMenus.update(menu.copyCSV,        {enabled:s.canCopy});
-            chrome.contextMenus.update(menu.copyText,       {enabled:s.canCopy});
+        chrome.contextMenus.update(menu.copyRich, {
+            enabled: s.canCopy
+        });
+        chrome.contextMenus.update(menu.copy, {
+            enabled: s.canCopy
+        });
+        chrome.contextMenus.update(menu.copyHTML, {
+            enabled: s.canCopy
+        });
+        chrome.contextMenus.update(menu.copyStyled, {
+            enabled: s.canCopy
+        });
+        chrome.contextMenus.update(menu.copyCSV, {
+            enabled: s.canCopy
+        });
+        chrome.contextMenus.update(menu.copyText, {
+            enabled: s.canCopy
+        });
 
-            sendResponse({});
-            break;
+        sendResponse({});
+        break;
     }
 });
-
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     var popupPage = chrome.extension.getViews({
-                type: "popup"
+        type: "popup"
     });
-    if(!popupPage) return;
+    if (!popupPage) return;
     popupPage = popupPage[0];
 
-
-    switch(message.popup_cmd) {
-        case "hasSelected":
-            popupPage.updateTablePreview(message.content,false);
-            popupPage.makeTableEditable();
-            break;
+    switch (message.popup_cmd) {
+    case "hasSelected":
+        popupPage.updateTablePreview(message.content, false);
+        popupPage.makeTableEditable();
+        break;
         // 语言设置
-        case "setLang0":
-            popupPage.updateLang(0);
-            break;
-        case "setLang1":
-            popupPage.updateLang(1);
-            break;
-            
+    case "setLang0":
+        popupPage.updateLang(0);
+        break;
+    case "setLang1":
+        popupPage.updateLang(1);
+        break;
+
         // 皮肤设置
-        case "setSkin0":
-            popupPage.changeSkin(0);
-            break;
-        case "setSkin1":
-            popupPage.changeSkin(1);
-            break;
-        case "setSkin2":
-            popupPage.changeSkin(2);
-            break;
-        case "setSkin3":
-            popupPage.changeSkin(3);
-            break;
-        case "setSkin4":
-            popupPage.changeSkin(4);
-            break;
+    case "setSkin0":
+        popupPage.changeSkin(0);
+        break;
+    case "setSkin1":
+        popupPage.changeSkin(1);
+        break;
+    case "setSkin2":
+        popupPage.changeSkin(2);
+        break;
+    case "setSkin3":
+        popupPage.changeSkin(3);
+        break;
+    case "setSkin4":
+        popupPage.changeSkin(4);
+        break;
     }
 });
-
